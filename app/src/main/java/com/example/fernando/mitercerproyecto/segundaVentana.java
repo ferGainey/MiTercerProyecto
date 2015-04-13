19e2256d358ec20c562fd8f3f7d73974646a01d3
ferGainey <fernandogainey@hotmail.com>
2015-04-13 16:55:11 -0300

    555

--- app/src/main/java/com/example/fernando/mitercerproyecto/segundaVentana.java
+++ app/src/main/java/com/example/fernando/mitercerproyecto/segundaVentana.java
@@ -1,18 +1,32 @@
 package com.example.fernando.mitercerproyecto;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class segundaVentana extends ActionBarActivity {
+
+    private EditText et1,et2;
+    private TextView tv4;
+    private CheckBox cb1,cb2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_ventana);
+        et1=(EditText)findViewById(R.id.et1);
+        tv4=(TextView)findViewById(R.id.tv4);
+        et2=(EditText)findViewById(R.id.et2);
+        cb1=(CheckBox)findViewById(R.id.cb1);
+        cb2=(CheckBox)findViewById(R.id.cb2);
     }
 
 
@@ -41,5 +55,36 @@ public class segundaVentana extends ActionBarActivity {
     public void salir(View view){
 
         finish();
+    }
+
+    public void operar(View view){
+        String valor1 = et1.getText().toString();
+        Double num1 = Double.parseDouble(valor1);
+        String valor2 = et2.getText().toString();
+        Double num2 = Double.parseDouble(valor2);
+        String resultado="";
+        int pantOculta=0;
+
+        if(cb1.isChecked()==true){
+            double suma = num1 + num2;
+            resultado="La suma es:" + suma;
+            if(suma==666){
+                pantOculta=1;
+            }
+        }
+
+        if(cb2.isChecked()==true){
+            double resta = num1 - num2;
+            resultado= resultado + "La resta es:" + resta;
+            if(resta==666){
+                pantOculta=1;
+            }
+        }
+        tv4.setText(resultado);
+        if(pantOculta==1){
+            Intent i = new Intent(this,VentanaOculta.class);
+            startActivity(i);
+        }
+
     }
 }
